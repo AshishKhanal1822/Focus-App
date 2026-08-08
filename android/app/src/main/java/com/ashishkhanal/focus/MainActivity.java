b79451cc4b8d8311040f8d99dc552b8375e35beb
AshishKhanal1822 <ashishkhanal1718@gmail.com>
2026-08-08 22:20:01 +0545

    Final

--- android/app/src/main/java/com/ashishkhanal/focus/MainActivity.java
+++ android/app/src/main/java/com/ashishkhanal/focus/MainActivity.java
@@ -1,5 +1,12 @@
 package com.ashishkhanal.focus;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(DoNotDisturbPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
-import { registerSW } from 'virtual:pwa-register'
+import { Capacitor } from '@capacitor/core'
 
-registerSW({
-  immediate: true,
-  onOfflineReady() {
-    console.log('PWA: App is ready to work offline')
-  }
-})
+// Only register service worker on web, NOT inside native Capacitor
+// Service workers interfere with Capacitor's local file serving and cause the app to get stuck
+if (!Capacitor.isNativePlatform()) {
+  import('virtual:pwa-register').then(({ registerSW }) => {
+    registerSW({
+      immediate: true,
+      onOfflineReady() {
+        console.log('PWA: App is ready to work offline')
+      }
+    })
+  })
+}
 
 import { dnd } from './utils/dnd.js'
 
